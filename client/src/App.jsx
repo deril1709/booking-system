fe0eac2c13d9f25d29cb1e7e5d7a9d065c66d56b
deril1709 <punktator17@gmail.com>
2023-09-14 17:38:28 +0800

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <a href="" className="flex items-center p-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
+          </svg>
+          <span className="font-semibold px-4">This is logo</span>
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
