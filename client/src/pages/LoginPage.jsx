5360cd72b90b179580557b29bd4b1d9f1aa4b6c0
deril1709 <punktator17@gmail.com>
2023-09-17 00:20:39 +0800

    login page design

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function LoginPage() {
     return (
-        <div>LoginPage</div>
+        <div className='mt-4 flex grow items-center justify-around'>
+            <div className='mb-40'>
+                <h1 className='text-4xl mb-4 text-center p-4'>Login</h1>
+                <form action="" className='max-w-md mx-auto '>
+                    <input type="text" placeholder='Email' />
+                    <input type="text" placeholder='Password' />
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
