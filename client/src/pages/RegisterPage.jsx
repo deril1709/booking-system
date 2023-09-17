7224f6a6cf5ef325a4b76bbac77f98d7a7b64647
deril1709 <punktator17@gmail.com>
2023-09-17 12:43:02 +0800

    setup express

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -7,6 +7,7 @@ function RegisterPage() {
             <div className='mb-40'>
                 <h1 className='text-4xl mb-4 text-center p-4'>Create Account</h1>
                 <form action="" className='max-w-md mx-auto '>
+                    <input type="text" placeholder='Username' />
                     <input type="text" placeholder='Email' />
                     <input type="text" placeholder='Password' />
                     <button>Register</button>
