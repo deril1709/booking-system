825942b9bc81383514f8dfbbaed73920b5a17827
deril1709 <punktator17@gmail.com>
2023-09-17 12:33:11 +0800

    Register Page

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -12,10 +12,9 @@ function Header() {
                     <span className="font-semibold text-xl">This is logo</span>
                 </a>
                 <div className='flex gap-2 border border-gray-300 rounded-full py-2 px-4 shadow-sm shadow-blue-500'>
-                    <div>Home</div>
-                    <div className='border-l border-gray-300'></div>
-                    <div>Lapangan</div>
-                    <div className='border-l border-gray-300'></div>
+                    <div>Damai</div>
+                    <div>Badminton</div>
+                    <div>Court</div>
                     <button className='bg-blue-400 text-white rounded-full p-1'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-4 h-4">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function LoginPage() {
+function RegisterPage() {
     return (
         <div className='mt-4 flex grow items-center justify-around'>
             <div className='mb-40'>
-                <h1 className='text-4xl mb-4 text-center p-4'>Login</h1>
+                <h1 className='text-4xl mb-4 text-center p-4'>Create Account</h1>
                 <form action="" className='max-w-md mx-auto '>
                     <input type="text" placeholder='Email' />
                     <input type="text" placeholder='Password' />
-                    <button>Login</button>
-                    <div className='text-center p-2'>Don't have account yet? <Link to={'/register'} className='underline text-gray-600'>Register Here!</Link>
+                    <button>Register</button>
+                    <div className='text-center p-2'>Already Have an Account? <Link to={'/login'} className='underline text-gray-600'>Login</Link>
                     </div>
                 </form>
             </div>
@@ -18,4 +18,4 @@ function LoginPage() {
     )
 }
 
-export default LoginPage
+export default RegisterPage
