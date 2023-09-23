03aab57790648e4e9f6a9cb95b577e27bd41dd3c
deril1709 <punktator17@gmail.com>
2023-09-23 15:35:36 +0800

    useState for loginPage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
+
 function LoginPage() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
     return (
         <div className='mt-4 flex grow items-center justify-around'>
             <div className='mb-40'>
                 <h1 className='text-4xl mb-4 text-center p-4'>Login</h1>
                 <form action="" className='max-w-md mx-auto '>
-                    <input type="email" placeholder='Email' />
-                    <input type="password" placeholder='Password' />
+                    <input type="email"
+                        placeholder='Email'
+                        value={email}
+                        onChange={e => setEmail(e.target.value)} />
+                    <input type="password"
+                        placeholder='Password'
+                        value={password}
+                        onChange={e => setPassword(e.target.value)} />
                     <button>Login</button>
                     <div className='text-center p-2'>Don't have an account yet? <Link to={'/register'} className='underline text-gray-600'>Register Here!</Link>
                     </div>
