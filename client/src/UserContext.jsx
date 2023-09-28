bddab919d93b7dd99afc4ed7f037e43d8063acf7
deril1709 <punktator17@gmail.com>
2023-09-28 15:21:57 +0800

    condition for user icon

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -1,10 +1,16 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import { useState } from "react";
+import axios from "axios";
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
     const [user, setUser] = useState(null);
+    useEffect(() => {
+        if (!user) {
+            axios.get('/profile')
+        }
+    }, [])
     return (
         <UserContext.Provider value={{ user, setUser }}>
             {children}
