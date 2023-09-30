212c956d286836df742c2a87904d1cbf34039e63
deril1709 <punktator17@gmail.com>
2023-09-30 20:01:36 +0800

    update design navbar

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react'
 import { UserContext } from '../UserContext'
-import { Navigate } from 'react-router-dom';
+import { Navigate, useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
 function AccountPage() {
@@ -14,15 +14,29 @@ function AccountPage() {
         return <Navigate to={'/login'} />;
     }
 
+
+    let { subpage } = useParams()
+    if (subpage === undefined) {
+        subpage = 'profile';
+    }
+
+
+    function linkClasses(type = null) {
+        let classes = 'py-2 px-6';
+        if (type === subpage) {
+            classes += 'text-white bg-blue-400 rounded-full';
+        }
+        return classes;
+    }
     return (
         <div>
-            <nav className='w-full justify-center mt-8 flex gap-4 '>
-                <Link className='p-2 px-6 text-white bg-blue-400' to={'/account'}>My profile</Link>
-                <Link className='p-2 px-6 ' to={'account/bookings'}>my booking</Link>
-                <Link className='p-2 px-6 ' to={'account/lapangan'}>Daftar Lapangan</Link>
+            <nav className='w-full flex justify-center mt-8  gap-2 '>
+                <Link className={linkClasses('profile')} to={'/account'}>My profile</Link>
+                <Link className={linkClasses('bookings')} to={'/account/bookings'}>my booking</Link>
+                <Link className={linkClasses('places')} to={'/account/lapangan'}>My accomondation</Link>
             </nav>
         </div>
     )
 }
 
-export default AccountPage
+export default AccountPage;
