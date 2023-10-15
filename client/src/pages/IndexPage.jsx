4a3299644b5c3b70e4ef47ff80a551c0687d2a4b
deril1709 <punktator17@gmail.com>
2023-10-15 14:18:55 +0800

    HomePage

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -4,18 +4,22 @@ import { Link } from 'react-router-dom';
 
 function IndexPage() {
     return (
-        <div className="min-h-screen flex flex-col md:flex-row items-center justify-center gap-5 mx-5 my-5">
+        <div className="min-h-screen flex flex-col md:flex-row items-center justify-center mx-5 my-5">
             <div className="w-full md:w-1/2">
-                <img src={img} alt="Badminton Image" className="rounded-lg shadow-lg" />
+                <img
+                    src={img}
+                    alt="Badminton Image"
+                    className="rounded-lg shadow-lg object-cover h-80 md:h-full w-full"
+                />
             </div>
             <div className="w-full md:w-1/2 text-center space-y-4">
-                <p className='text-justify text-4xl font-extrabold'>DAMAI BOOKING APP</p>
-                <p className="text-lg text-start text-gray-700">
+                <h1 className="text-5xl font-poppins text-blue-400">DAMAI BOOKING APP</h1>
+                <p className="text-lg text-gray-700">
                     Mudahkan pengalaman bermain anda dengan mulai memesan lapangan <br />
                     melalui aplikasi pembookingan lapangan Badminton.
                 </p>
-                <Link to="/home">
-                    <button className="p-3 bg-blue-400 text-white text-lg rounded-md hover:bg-blue-600 mt-6">
+                <Link to="/login">
+                    <button className="p-4 bg-blue-400 text-white text-lg rounded-lg hover:bg-blue-600 hover:shadow-md transition duration-300 mt-6">
                         Book Now
                     </button>
                 </Link>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,9 +5,11 @@ export default {
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
       colors: {}
     },
   },
   plugins: [],
 }
-
+}
