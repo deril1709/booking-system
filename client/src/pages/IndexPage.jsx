848ab2a73918be527e2bfe0334cae9fc86577ae7
deril1709 <punktator17@gmail.com>
2024-03-14 20:14:30 +0800

    Update: Design HomePage

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -9,17 +9,18 @@ function IndexPage() {
                 <img
                     src={img}
                     alt="Badminton Image"
-                    className="rounded-lg shadow-lg object-cover object-center h-50 md:h-full w-full"
+                    className="rounded-lg shadow-lg object-cover object-center h-80 md:h-full w-full"
                 />
             </div>
-            <div className="w-full md:w-1/2 text-center space-y-4 mt-4 md:mt-0">
-                <h1 className="text-5xl font-montserrat italic text-blue-400">DAMAI BOOKING APP</h1>
-                <p className="text-lg font-montserrat font-thin text-black">
-                    Mudahkan pengalaman bermain anda dengan mulai memesan lapangan <br />
-                    melalui aplikasi pembookingan lapangan Badminton.
+            <div className="w-full md:w-1/2 flex flex-col justify-center items-center md:items-start space-y-4 mt-4 md:mt-0">
+                <h1 className="text-4xl md:text-5xl font-bold italic text-blue-500 text-center md:text-left">
+                    DAMAI BOOKING APP
+                </h1>
+                <p className="text-lg md:text-xl font-thin text-gray-800 text-center md:text-left">
+                    Mudahkan pengalaman bermain Anda dengan mulai memesan lapangan melalui aplikasi pembookingan lapangan Badminton.
                 </p>
-                <Link to="/list">
-                    <button className="p-4 bg-blue-400 text-white text-lg rounded-lg hover:bg-blue-600 hover:shadow-md transition duration-300 mt-6">
+                <Link to="/list" className="text-center md:text-left">
+                    <button className="p-4 bg-blue-500 hover:bg-blue-600 text-white text-lg font-semibold rounded-lg shadow-md transition duration-300">
                         Lihat Lapangan
                     </button>
                 </Link>
